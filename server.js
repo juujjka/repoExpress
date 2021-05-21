e9513d130da8ae95ab0bd16f10623e298904c556
juujjka <just.fraczek@gmail.com>
2021-05-21 08:46:12 +0200

    del

--- server.js
+++
@@ -1,7 +0,0 @@
-
-const express = require('express');
-const app = express();
-// const bodyHospital = require('body-parser');
-app.use(bodyHospital.json())
-
-  console.log("App listening at http://%s:%s", host, port)
